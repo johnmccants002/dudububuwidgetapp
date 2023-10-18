cd5e5e371929b22f4fba2b067bb37469d48f57dd
John McCants <47401369+johnmccants002@users.noreply.github.com>
2023-10-18 02:00:23 -0700

    Initial Commit

---
+++ DuduBubu/DuduBubuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuduBubuApp.swift
+//  DuduBubu
+//
+//  Created by John McCants on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct DuduBubuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
